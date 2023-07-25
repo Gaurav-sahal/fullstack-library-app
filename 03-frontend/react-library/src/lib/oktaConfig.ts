38bfd664989d4ec77f2d0c8d413a287988a5dad0
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-07-25 23:53:20 +0530

    Added oktaConfig

---
+++ 03-frontend/react-library/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: '0oaaipnl86X3XQZ1U5d7',
+    issuer: 'https://dev-93918451.okta.com/oauth2/default',
+    redirectUi: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+}
