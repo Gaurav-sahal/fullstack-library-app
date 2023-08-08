b64b2f354ddaa4d593705f2089ba69dae3f9c529
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-08-08 11:11:59 +0530

    Updated ShelfPage

--- 03-frontend/react-library/src/layouts/ShelfPage/ShelfPage.tsx
+++ 03-frontend/react-library/src/layouts/ShelfPage/ShelfPage.tsx
@@ -1,17 +1,22 @@
+import { HistoryPage } from "./components/HistoryPage";
 import { Loans } from "./components/Loans";
+import { useState } from "react";
 
 export const ShelfPage = () => {
+
+    const [historyClick, setHistoryClick] = useState(false);
+
     return (
         <div className="container">
             <div className="mt-3">
                 <nav>
                     <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                        <button className="nav-link active" id="nav-loans-tab" data-bs-toggle="tab"
+                        <button onClick={() => setHistoryClick(false)} className="nav-link active" id="nav-loans-tab" data-bs-toggle="tab"
                             data-bs-target="#nav-loans" type="button" role="tab" aria-controls="nav-loans"
                             aria-selected="true">
                                 Loans
                         </button>
-                        <button className="nav-link" id="nav-loans-tab" data-bs-toggle="tab"
+                        <button onClick={() => setHistoryClick(true)} className="nav-link" id="nav-loans-tab" data-bs-toggle="tab"
                             data-bs-target="#nav-history" type="button" role="tab" aria-controls="nav-history"
                             aria-selected="false">
                                 Your History
@@ -21,12 +26,11 @@ export const ShelfPage = () => {
                 <div className="tab-content" id="nav-tabContent">
                     <div className="tab-pane fade show active" id="nav-loans" role="tabpanel"
                         aria-labelledby="nav-loans-tab">
-                            {/* <p>Loans</p> */}
                             <Loans />
                     </div>
                     <div className="tab-pane fade" id="nav-history" role="tabpanel"
                         aria-labelledby="nav-history-tab">
-                            <p>Checkout History</p>
+                            {historyClick ? <HistoryPage /> : <></>}
                     </div>
                 </div>
             </div>
