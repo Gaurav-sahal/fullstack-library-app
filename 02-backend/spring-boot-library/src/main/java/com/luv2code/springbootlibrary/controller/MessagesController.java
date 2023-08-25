39d612698cd00486485ed4acd28ea3c7ba6ceebd
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-08-25 22:54:57 +0530

    Updated MessageController

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/MessagesController.java
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/controller/MessagesController.java
@@ -1,12 +1,14 @@
 package com.luv2code.springbootlibrary.controller;
 
 import com.luv2code.springbootlibrary.entity.Message;
+import com.luv2code.springbootlibrary.requestmodels.AdminQuestionRequest;
 import com.luv2code.springbootlibrary.service.MessagesService;
 import com.luv2code.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://localhost:3000")
 @RestController
 @RequestMapping("/api/messages")
 public class MessagesController {
@@ -23,5 +25,16 @@ public class MessagesController {
                             @RequestBody Message messageRequest) {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messagesService.postMessage(messageRequest, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        messagesService.putMessage(adminQuestionRequest, userEmail);
     }
 }
