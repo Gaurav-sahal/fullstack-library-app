4ce8d453b560ff1971117247c697524be6725c94
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-12-02 21:52:21 +0530

    Modified index.tsx

--- 03-frontend/react-library/src/index.tsx
+++ 03-frontend/react-library/src/index.tsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_test_51Nlr7TSI9gFQX59bkoeFhEIGPvokvbxlXvWKi7UeLDzuumuNBwV8KuUfdkMyfcPjeauoLq0Tt4tvxl6tepmPzMeL00JBm3OqeF');
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </BrowserRouter>
 );
